1b6c6323138254bff59338dc52fcc077039fe513
Kai Rune Orten <krorten@gmail.com>
2018-07-21 18:58:07 +0200

    Update

--- adrenaline/install.sh
+++ adrenaline/install.sh
@@ -4,7 +4,7 @@ clear
 
 cd ~
 echo "***********************************************************"
-echo "* Welcome to Kayroons HENGA masternode install script *"
+echo "* Welcome to Kayroons ADRENALINE masternode install script *"
 echo "*                                                         *"                                                        
 echo "*                   Powered by Kayroons                   *"                                
 echo "***********************************************************"
@@ -39,7 +39,6 @@ sudo add-apt-repository ppa:bitcoin/bitcoin -y
 sudo apt-get update -y
 sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
 sudo apt-get install libminiupnpc-dev -y
-sudo apt-get install libzmq5 -qq -y
 sudo apt-get install -y git python-virtualenv
 
 echo && echo "${bold}Installing UFW...${regular}"
@@ -50,25 +49,27 @@ sleep 3
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow ssh
-sudo ufw allow 19441/tcp
+sudo ufw allow 23202/tcp
 echo "y" | sudo ufw enable
 echo && echo "${bold}Firewall installed and enabled!${regular}"
 
 echo && echo
-echo "${bold}Downloading and installing HENGA Files${regular}"
-wget https://github.com/HengaCoin/henga/releases/download/v1.0/henga-cli
-wget https://github.com/HengaCoin/henga/releases/download/v1.0/hengad
-sudo cp henga{d,-cli} /usr/local/bin
-sudo chmod +x /usr/local/bin/henga{d,-cli}
+echo "${bold}Downloading and installing ADRENALINE Files${regular}"
+sudo wget https://github.com/adrenaline-project/adn/releases/download/v1.0.0.14/adn-cli_linux64
+sudo wget https://github.com/adrenaline-project/adn/releases/download/v1.0.0.14/adnd_linux64
+sudo cp ~/adnd_linux64 /root/adnd
+sudo cp ~/adn-cli_linux64 /root/adn-cli
+sudo chmod 777 /root/adnd
+sudo chmod 777 /root/adn-cli
+sudo cp adn{d,-cli} /usr/local/bin
 
 sleep 3
 echo && echo "${bold}Setting config${regular}"
 rpcuser=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
 rpcpassword=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
 IP_ADD=`curl ipinfo.io/ip`
-cd /root
-mkdir -p .henga
-sudo touch .henga/henga.conf
+mkdir -p .adn
+sudo touch .adn/adn.conf
 echo '
 rpcuser='$rpcuser'
 rpcpassword='$rpcpassword'
@@ -81,13 +82,13 @@ maxconnections=250
 externalip='$IP_ADD'
 masternodeprivkey='$key'
 masternode=1
-' | sudo -E tee /root/.henga/henga.conf
+' | sudo -E tee /root/.adn/adn.conf
 sleep 3
-echo && echo "${bold}Starting HENGA Deamon...${regular}"
-henga-cli stop
+echo && echo "${bold}Starting ADRENALINE Deamon...${regular}"
+adn-cli stop
 sleep 10
-hengad -deamon &
+adnd --deamon &
 sleep 3
-echo && echo "${bold}Checking HENGA Deamon...${regular}"
-henga-cli getinfo
+echo && echo "${bold}Checking ADRENALINE Deamon...${regular}"
+ADRENALINE-cli getinfo
 echo && echo "${bold}Have a beer and enjoy! Masternode setup is complete.${regular}"
